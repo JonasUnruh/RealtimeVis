b10cd7e5fa1d44987f26cf6da35fbaa1dc70d2c2
JonasUnruh <Jonas.unruh@t-online.de>
2024-12-24 16:50:26 +0100

    added more functionality back

--- app.py
+++ app.py
@@ -16,7 +16,22 @@ state_summary = state_summary.select(
     pl.col("accidents").cast(pl.Int32),
     pl.col("avg_severity").cast(pl.Float64)
 )
-print(state_summary.filter(pl.col("State") == "CA"))
+
+severity_summary = pl.read_parquet("./assets/data/severity_level.parquet")
+
+severity_summary = severity_summary.select(
+    pl.col("State"),
+    pl.col("Severity").cast(pl.Int32),
+    pl.col("len").cast(pl.Int32)
+)
+
+monthly_accidents = pl.read_parquet("./assets/data/monthly_accidents.parquet")
+
+monthly_accidents = monthly_accidents.select(
+    pl.col("State"),
+    pl.col("Month").cast(pl.Date),
+    pl.col("accidents").cast(pl.Int32)
+)
 
 with open("./assets/data/us-states.json", "r") as f:
     geojson = json.loads(f.read())
@@ -40,46 +55,33 @@ def create_choropleth_map():
         mapbox_accesstoken=mapbox_access_token,
         mapbox_style="dark",
         mapbox_zoom=3,
-        mapbox_center={"lat": 38.000000, "lon": -90.000000},
-        font=dict(
-            color="white"
-        )
+        mapbox_center={"lat": 38.000000, "lon": -90.000000}
     )
 
     return fig
 
 
 # Create the bar chart for severity levels
-""" def create_severity_bar_chart():
-    severity_counts = df_sampled['Severity'].value_counts().reset_index()
-    severity_counts.columns = ['Severity', 'Accident_Count']
-    severity_counts = severity_counts.sort_values('Severity')  # Ensure proper order
-    fig = px.bar(severity_counts, x='Severity', y='Accident_Count',
+def create_severity_bar_chart():
+    fig = px.bar(severity_summary.filter(pl.col("State") == "All States"), x='Severity', y='len',
                  title='Accidents by Severity',
-                 labels={'Accident_Count': 'Number of Accidents'},
+                 labels={'len': 'Number of Accidents'},
                  template='plotly_dark',
                  color_discrete_sequence=['orange'])
-    return fig """
+    return fig 
 
 
 # Create the line graph for accidents over time (aggregated by month)
-""" def create_accidents_over_month_line_graph():
-    # Convert 'Start_Time' to datetime and extract the month
-    df_sampled['Month'] = pd.to_datetime(df_sampled['Start_Time'], format='mixed', errors='coerce').dt.to_period('M')
-    accidents_over_month = df_sampled.groupby('Month').size().reset_index(name='Accident_Count')
-
-    # Convert 'Month' Period objects to strings for JSON serialization
-    accidents_over_month['Month'] = accidents_over_month['Month'].astype(str)
-
+def create_accidents_over_month_line_graph():
     # Plot the line graph
-    fig = px.line(accidents_over_month, x='Month', y='Accident_Count',
+    fig = px.line(monthly_accidents.filter(pl.col("State") == "All States"), x='Month', y='accidents',
                   title='Accidents Over Time (Monthly)',
-                  labels={'Accident_Count': 'Number of Accidents', 'Month': 'Month'},
+                  labels={'accidents': 'Number of Accidents', 'Month': 'Month'},
                   template='plotly_dark')
     fig.update_traces(line=dict(color='orange'))  # Set line color to orange
     fig.update_xaxes(title_text='Month', tickangle=45)  # Rotate x-axis labels for readability
     fig.update_yaxes(title_text='Number of Accidents')  # Update y-axis label
-    return fig """
+    return fig
 
 
 app.layout = [
@@ -97,7 +99,14 @@ app.layout = [
                             html.Div(
                                 className="div-dropdown",
                                 children=[
-                                    dcc.Dropdown()
+                                    dcc.Dropdown(
+                                        id='chart-type-dropdown',
+                                        options=[
+                                            {'label': 'Severity Bar Chart', 'value': 'severity_bar'},
+                                            {'label': 'Accidents Over Time Line Graph', 'value': 'time_line'}
+                                        ],
+                                        value='severity_bar',  # Default value
+                                    )
                                 ]
                             )
                         ]
@@ -126,7 +135,7 @@ app.layout = [
 ]
 
 
-""" @app.callback(
+@app.callback(
     Output('chart-display', 'figure'),
     Input('chart-type-dropdown', 'value')
 )
@@ -135,7 +144,7 @@ def update_chart(chart_type):
         return create_severity_bar_chart()
     elif chart_type == 'time_line':
         return create_accidents_over_month_line_graph()  
- """
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,47 @@ import polars as pl
 
 data = pl.read_csv("./assets/data/US_Accidents_March23.csv")
 
-state_summary = data.group_by('State').agg([
+""" state_summary = data.group_by('State').agg([
     pl.count("ID").alias("accidents"),
     pl.mean("Severity").alias("avg_severity")
 ])
 
-state_summary.write_parquet("./assets/data/state_level.parquet")
+state_summary.write_parquet("./assets/data/state_level.parquet") """
+
+""" severity_summary = data.group_by(["State", "Severity"]).len()
+
+all_states = severity_summary.group_by("Severity").agg(
+    pl.sum("len").alias("len")
+).with_columns(
+    pl.lit("All States").alias("State")
+)
+
+all_states = all_states.select(severity_summary.columns)
+
+result = pl.concat([severity_summary, all_states])
+
+result.write_parquet("./assets/data/severity_level.parquet") """
+
+data = data.with_columns(
+    pl.col("Start_Time")
+    .str.strptime(pl.Datetime, strict=False)
+    .dt.truncate("1mo")
+    .alias("Month")
+)
+
+accidents_monthly = data.group_by(['State', 'Month']).agg([
+    pl.count("ID").alias("accidents")
+])
+
+all_states = accidents_monthly.group_by("Month").agg(
+    pl.sum("accidents").alias("accidents")
+).with_columns(
+    pl.lit("All States").alias("State")
+)
+
+all_states = all_states.select(accidents_monthly.columns)
+
+result = pl.concat([accidents_monthly, all_states])
+result = result.sort("Month")
+
+result.write_parquet("./assets/data/monthly_accidents.parquet")
